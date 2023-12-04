6796ca5ad7de603e7c5ccdd93f4050c6d32f62ca
vinicius <vinicius.teske@hotmail.com>
2023-12-03 21:15:30 -0300

    done

--- mpi-exercise-3.cpp
+++ mpi-exercise-3.cpp
@@ -8,11 +8,12 @@ using namespace std;
 
 int main(int argc, char **argv){
     int rank, size;
-    int p;
+    int bigger = 0;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 	int n = atoi(argv[1]);
+    int p = atoi(argv[2]);
     char **array = (char **)malloc(n *sizeof(char*));
     for(int i=0; i<n; ++i)
     	array[i] = (char *)malloc(MAX*sizeof(char));
@@ -24,18 +25,23 @@ int main(int argc, char **argv){
                 strcpy(array[i], buffer);
             }
         }
+        char *send_buffer = (char *)malloc(n * MAX * sizeof(char));
+        if (rank == 0) {
+            for (int i = 0; i < n; ++i) {
+                strcpy(send_buffer + i * MAX, array[i]);
+            }
+        }
         char *local_array = (char *)malloc(MAX*sizeof(char));
-        MPI_Scatter(array, MAX, MPI_CHAR, local_array, MAX, MPI_CHAR, 0, MPI_COMM_WORLD);
-        // for(int i=0; i<n; ++i)
-		// 	MPI_Bcast(array[i], MAX, MPI_CHAR, 0, MPI_COMM_WORLD);	
+        MPI_Scatter(send_buffer, MAX, MPI_CHAR, local_array, MAX, MPI_CHAR, 0, MPI_COMM_WORLD);
+        int local = 0;
+        if(strlen(local_array) > p)
+            local += 1;
+        MPI_Reduce(&local, &bigger, 1, MPI_INT, MPI_SUM, 1, MPI_COMM_WORLD);
+        if(rank == 1)
+            printf("%d\n", bigger);
         
-        printf("%d ", rank);
-        printf("%s\n", local_array);
-        // for(int j=0; j<n; ++j){
-        //     printf("%s ", array[j]);
-        // }
-        printf("\n");
         free(local_array);
+        free(send_buffer);
         for(int i=0; i<n; ++i)
         	free(array[i]);
         free(array);
